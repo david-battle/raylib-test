7827fa5fe780a2f5e3411610ade22ac4c9ad8be1
David Battle <dlbattle@gmail.com>
2026-08-07 18:21:56 -0400

    Add drifting color-keyed sprite to fullscreen test

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include <stdio.h>
+#include <math.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -16,6 +17,17 @@ int main(void) {
     Sound echoSound = LoadSound("resources/coin.wav");
     Sound clickSound = LoadSound("resources/buttonfx.wav");
 
+    Image spriteImg = LoadImage("resources/sprite.png");
+    for (int y = 0; y < spriteImg.height; y++) {
+        for (int x = 0; x < spriteImg.width; x++) {
+            Color px = GetImageColor(spriteImg, x, y);
+            if (px.r > 200 && px.g < 80 && px.b > 200)
+                ImageDrawPixel(&spriteImg, x, y, (Color){ px.r, px.g, px.b, 0 });
+        }
+    }
+    Texture2D sprite = LoadTextureFromImage(spriteImg);
+    UnloadImage(spriteImg);
+
     // Non-blocking UDP socket
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
     fcntl(sock, F_SETFL, O_NONBLOCK);
@@ -26,6 +38,8 @@ int main(void) {
 
     Rectangle clickBox = { 100, 300, 150, 80 };
     bool boxHeld = false;
+    Vector2 spritePos = { 180, 340 };
+    Vector2 spriteVel = { 1.5f, 0.8f };
     int packetsSent = 0, packetsReceived = 0;
 
     while (!WindowShouldClose()) {
@@ -51,18 +65,38 @@ int main(void) {
             PlaySound(clickSound);
         }
 
+        float vx = spriteVel.x + GetRandomValue(-10, 10)*0.01f;
+        float vy = spriteVel.y + GetRandomValue(-10, 10)*0.01f;
+        float dx = mouse.x - (spritePos.x + 32);
+        float dy = mouse.y - (spritePos.y + 32);
+        float dist = sqrtf(dx*dx + dy*dy);
+        if (dist > 1.0f) {
+            vx += (dx/dist)*0.04f;
+            vy += (dy/dist)*0.04f;
+        }
+        spriteVel.x = vx < -3.0f ? -3.0f : (vx > 3.0f ? 3.0f : vx);
+        spriteVel.y = vy < -3.0f ? -3.0f : (vy > 3.0f ? 3.0f : vy);
+        spritePos.x += spriteVel.x;
+        spritePos.y += spriteVel.y;
+        if (spritePos.x < -64) spritePos.x = GetScreenWidth();
+        else if (spritePos.x > GetScreenWidth()) spritePos.x = -64;
+        if (spritePos.y < -64) spritePos.y = GetScreenHeight();
+        else if (spritePos.y > GetScreenHeight()) spritePos.y = -64;
+
         BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawText("Press SPACE to send UDP ping", 100, 100, 30, DARKGRAY);
             DrawText(TextFormat("Sent: %d   Received: %d", packetsSent, packetsReceived), 100, 150, 20, GRAY);
             DrawRectangleRec(clickBox, boxHeld ? SKYBLUE : LIGHTGRAY);
             DrawText("Click me", clickBox.x + 20, clickBox.y + 30, 20, DARKGRAY);
+            DrawTexture(sprite, spritePos.x, spritePos.y, WHITE);
             DrawFPS(10, 10);
         EndDrawing();
     }
 
     UnloadSound(echoSound);
     UnloadSound(clickSound);
+    UnloadTexture(sprite);
     CloseAudioDevice();
     CloseWindow();
     return 0;
